d23fe262ab776c81a4026fc9846f07a315db2983
AliaksandrL <lipskialiaksandr@gmail.com>
2021-11-29 21:25:41 +0300

    refactoring store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,18 @@
-import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
-import {todoListsReducer} from '../features/TodolistsList/todolists-reducer';
-import {applyMiddleware, combineReducers, createStore} from 'redux'
-import {appReducer} from "./app-reducer";
+import { tasksReducer } from '../features/TodolistsList/tasks-reducer';
+import { todolistsReducer } from '../features/TodolistsList/todolists-reducer';
+import { applyMiddleware, combineReducers, createStore } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { appReducer } from './app-reducer'
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
 const rootReducer = combineReducers({
     tasks: tasksReducer,
-    todolists: todoListsReducer,
-    app: appReducer,
+    todolists: todolistsReducer,
+    app: appReducer
 })
 // непосредственно создаём store
-export const store = createStore(rootReducer, applyMiddleware());
+export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
