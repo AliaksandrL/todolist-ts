3bb86ca22745eba4127271f230d107519a00d43b
AliaksandrL <lipskialiaksandr@gmail.com>
2022-02-03 18:32:34 +0300

    add Login

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -1,16 +1,17 @@
 import axios, {AxiosResponse} from "axios";
+import {GetTasksResponse, LoginParamsType, ResponseType, TaskType, TodolistType, UpdateTaskModelType} from './types'
 
 
 const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        'API-KEY': 'ae3793e8-7c42-49d3-82ac-f60f84efebfe'
+        'API-KEY': 'e2b857bb-4c54-4e72-81b4-dd1c708c4455'
     }
 })
 
 
-export const api = {
+export const todolistsAPI = {
     getTodolists() {
         return instance.get<TodolistType[]>('todo-lists');
     },
@@ -37,58 +38,79 @@ export const api = {
     }
 }
 
-// types
-export type TodolistType = {
-    id: string
-    title: string
-    addedDate: string
-    order: number
+export const authAPI = {
+    login(data: LoginParamsType) {
+        // @ts-ignore
+        return instance.post<ResponseType<{userId?: number | undefined}>>('auth/login', data);
+    },
+    logout() {
+        return instance.delete<ResponseType<{userId?: number}>>('auth/login');
+    },
+    me() {
+        return instance.get<ResponseType<{id: number; email: string; login: string}>>('auth/me');
+    }
 }
-export type ResponseType<D = {}> = {
-    resultCode: number
-    messages: Array<string>
-    fieldsErrors: Array<string>
-    data: D
-}
-
-
-export enum TaskStatuses {
-    New = 0,
-    InProgress = 1,
-    Completed = 2,
-    Draft = 3
-}
-
-export enum TaskPriorities {
-    Low = 0,
-    Middle = 1,
-    Hi = 2,
-    Urgently = 3,
-    Later = 4
-}
-
-export type TaskType = {
-    description: string
-    title: string
-    status: TaskStatuses
-    priority: TaskPriorities
-    startDate: string
-    deadline: string
-    id: string
-    todoListId: string
-    order: number
-    addedDate: string
-}
-export type UpdateTaskModelType = {
-    title: string
-    description: string
-    status: TaskStatuses
-    priority: TaskPriorities
-    startDate: string
-    deadline: string
-}
-type GetTasksResponse = {
-    error: string | null
-    totalCount: number
-    items: TaskType[]
-}
+//
+// // types
+// export type LoginParamsType = {
+//     email: string
+//     password: string
+//     rememberMe: boolean
+//     captcha?: string
+// }
+//
+// export type TodolistType = {
+//     id: string
+//     title: string
+//     addedDate: string
+//     order: number
+// }
+// export type FieldErrorType = { field: string; error: string }
+// export type ResponseType<D = {}> = {
+//     resultCode: number
+//     messages: Array<string>
+//     fieldsErrors?: Array<FieldErrorType>
+//     data: D
+// }
+//
+//
+// export enum TaskStatuses {
+//     New = 0,
+//     InProgress = 1,
+//     Completed = 2,
+//     Draft = 3
+// }
+//
+// export enum TaskPriorities {
+//     Low = 0,
+//     Middle = 1,
+//     Hi = 2,
+//     Urgently = 3,
+//     Later = 4
+// }
+//
+// export type TaskType = {
+//     description: string
+//     title: string
+//     status: TaskStatuses
+//     priority: TaskPriorities
+//     startDate: string
+//     deadline: string
+//     id: string
+//     todoListId: string
+//     order: number
+//     addedDate: string
+// }
+// export type UpdateTaskModelType = {
+//     title: string
+//     description: string
+//     status: TaskStatuses
+//     priority: TaskPriorities
+//     startDate: string
+//     deadline: string
+// }
+// export type GetTasksResponse = {
+//     error: string | null
+//     totalCount: number
+//     items: TaskType[]
+// }
